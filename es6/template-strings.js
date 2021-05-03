91964ab2ffb6bfdb4e3d8fdbf8dde28fa3139656
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-03 13:58:05 +0530

    Template Strings

---
+++ es6/template-strings.js
@@ -0,0 +1,8 @@
+const name = 'Bill';
+const age = 35;
+
+// const sentence = 'My name is ' + name + ', and I am ' + age + ' years old';
+
+const sentence = `My name is ${name}, and I am ${age * 2} years old`;
+
+console.log(sentence);
