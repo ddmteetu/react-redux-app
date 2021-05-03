54dad2b8badced0176ab52d9d9a5aed6e43698dd
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-03 16:04:04 +0530

    2 adjacent 'JSX' elements must be wrapped within a single element

--- photowall/src/index.js
+++ photowall/src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 
 const tasks = ['Take out the trash', 'Shovel the driveway', 'Walk the dog'];
 
-const element = <ol>
-  { tasks.map((task, index) => <li key = {index}>{task}</li>) }
-</ol>
+const element =
+  <div>
+    <h1>Task List</h1>
+    <ol>
+      { tasks.map((task, index) => <li key = {index}>{task}</li>) }
+    </ol>
+  </div>
 
 ReactDOM.render(element, document.getElementById('root'));
