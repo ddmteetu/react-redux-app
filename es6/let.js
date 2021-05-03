fc05808b6f00ad0afb2a3065d68e6210355468ef
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-03 13:25:37 +0530

    'let' variables are 'block' scoped

--- es6/let.js
+++ es6/let.js
@@ -16,7 +16,8 @@ printName();*/
 var name3 = "Vishvendra";
 
 if (name3 == "Vishvendra") {
-  var fullName = "Vishvendra Singh";
+  let fullName = "Vishvendra Singh";
 }
 
+// 'let' variables are 'block' scoped, can not access below
 console.log(fullName); // 'var' is 'function' scoped
