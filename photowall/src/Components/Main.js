afdfff963e3d4019961ee80b89f7bc975d23aa56
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-03 16:55:29 +0530

    Removing 'List' Component and updating the project files

--- photowall/src/Components/Main.js
+++ photowall/src/Components/Main.js
@@ -1,14 +1,11 @@
 import React, {Component} from 'react';
 import Title from './Title';
-import List from './List';
 
 class Main extends Component {
   render() {
     return (
       <div>
-        <Title title = {'To Dos'} />
-        <List tasks = {['Mow the Lawn', 'Walk the Dog']} />
-        <List tasks = {['Hose the Driveway', 'Finish the Laundry']} />
+        <Title title = {'Photowall'} />
       </div>
     );
   }
