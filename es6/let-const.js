47aad2530f3bd157dabc585fb1322dc0fbba2388
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-03 13:34:56 +0530

    'const' variable can not be updated

--- es6/let-const.js
+++ es6/let-const.js
@@ -3,6 +3,7 @@ var name = "Vishvendra";
 if (name == "Vishvendra") {
   let fullName = "Vishvendra Singh";
   const age = 21;
+  age = 41; // throws an error as 'const' variable can not be updated
 }
 
 console.log(age); // not accessible as 'const' is 'block' scoped
