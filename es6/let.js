3e51a3cc333a4fc7693c7cebe3fb83b8b375b31d
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-03 13:24:17 +0530

    'var' is 'function' scoped

--- es6/let.js
+++ es6/let.js
@@ -2,3 +2,21 @@ var name = "Vishvendra Singh";
 console.log(name);
 name = "Rayan";
 console.log(name);
+
+
+// Scopes
+/* function printName() {
+	var name2 = "Vishvendra Singh";
+  	console.log(name2);
+}
+console.log(name2);
+
+printName();*/
+
+var name3 = "Vishvendra";
+
+if (name3 == "Vishvendra") {
+  var fullName = "Vishvendra Singh";
+}
+
+console.log(fullName); // 'var' is 'function' scoped
