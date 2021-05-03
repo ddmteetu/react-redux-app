dab5d84887be355fc3490e73e12abe8a2fbbff58
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-03 13:33:10 +0530

    'const' is 'block' scoped

---
+++ es6/let-const.js
@@ -0,0 +1,8 @@
+var name = "Vishvendra";
+
+if (name == "Vishvendra") {
+  let fullName = "Vishvendra Singh";
+  const age = 21;
+}
+
+console.log(age); // not accessible as 'const' is 'block' scoped
