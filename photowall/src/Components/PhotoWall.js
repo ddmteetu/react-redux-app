fdde1a20fc47a94a5d94eddefedb883dc0bad909
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-04 15:22:17 +0530

    adding 'Link Component' to manage different URL paths

--- photowall/src/Components/PhotoWall.js
+++ photowall/src/Components/PhotoWall.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Photo from './Photo';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 // anchor tag, href attribute
 function PhotoWall(props) {
   return (
     <div>
-      <a onClick={props.onNavigate()} href="#AddPhoto" className="add-icon"></a>
-      {/*<button className="add-icon" onClick={props.onNavigate()}></button>*/}
+      <Link to="/AddPhoto" className="add-icon"></Link>
       <div className="photo-grid">
         {props.posts.map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto} />)}
       </div>
