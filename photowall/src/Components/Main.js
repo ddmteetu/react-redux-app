35ff25c0e85a1b598ec552b7a874dec986b4d33d
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-04 14:13:28 +0530

    working with 'Lifecycle Hooks'

--- photowall/src/Components/Main.js
+++ photowall/src/Components/Main.js
@@ -6,7 +6,20 @@ class Main extends Component {
   constructor() {
     super();
     this.state = {
-      posts: []
+      posts: [{
+           id: "0",
+           description: "beautiful landscape",
+           imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
+           "3919321_1443393332_n.jpg"
+         }, {
+           id: "1",
+           description: "Aliens???",
+           imageLink: "https://s3.india.com/wp-content/uploads/2017/12/rocket.jpg"
+         }, {
+           id: "2",
+           description: "On a vacation!",
+           imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
+       }]
     };
     this.removePhoto = this.removePhoto.bind(this);
     console.log('constructor');
@@ -20,10 +33,6 @@ class Main extends Component {
   }
 
   componentDidMount() {
-    const data = SimulateFetchFromDatabase();
-    this.setState({
-      posts: data
-    });
     console.log('componentDidMount');
   }
 
@@ -32,8 +41,9 @@ class Main extends Component {
     // before the render method is called
   }
 
-  componentDidUpdate() {
-    alert('re-render');
+  componentDidUpdate(prevProps, prevState) {
+    console.log(prevState.posts);
+    console.log(this.state);
   }
 
   render() {
@@ -45,23 +55,6 @@ class Main extends Component {
       </div>
     );
   }
-}
-
-function SimulateFetchFromDatabase() {
-  return [{
-       id: "0",
-       description: "beautiful landscape",
-       imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
-       "3919321_1443393332_n.jpg"
-     }, {
-       id: "1",
-       description: "Aliens???",
-       imageLink: "https://s3.india.com/wp-content/uploads/2017/12/rocket.jpg"
-     }, {
-       id: "2",
-       description: "On a vacation!",
-       imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
-   }];
 }
 
 export default Main;
