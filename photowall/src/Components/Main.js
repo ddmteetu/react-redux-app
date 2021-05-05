355c32fdb061aadd2232932cc81e5188c509f863
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-05 17:11:21 +0530

    Adding posts and updating state using 'Redux'

--- photowall/src/Components/Main.js
+++ photowall/src/Components/Main.js
@@ -3,7 +3,7 @@ import Title from './Title';
 import PhotoWall from './PhotoWall';
 import AddPhoto from './AddPhoto';
 import {Route} from 'react-router-dom';
-import {removePost} from '../redux/actions';
+import {Link} from 'react-router-dom';
 
 class Main extends Component {
   constructor() {
@@ -14,18 +14,17 @@ class Main extends Component {
     console.log(this.props);
     return (
       <div>
+        <h1>
+          <Link to="/">Photowall</Link>
+        </h1>
         <Route exact path="/" render={() => (
           <div>
-            <Title title={'Photowall'} />
             <PhotoWall {...this.props} />
           </div>
         )} />
-        {/*<Route path="/AddPhoto" render={({history}) => (
-          <AddPhoto onAddPhoto={(addedPost) => {
-            this.addPhoto(addedPost);
-            history.push('/');
-          }} />
-        )} />*/}
+        <Route path="/AddPhoto" render={({history}) => (
+          <AddPhoto {...this.props} onHistory={history} />
+        )} />
       </div>
     );
   }
