ae98f5ab173b5745ac599dace666958fe185d4a8
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-06 13:01:23 +0530

    adding comments and managing state

--- photowall/src/redux/reducer.js
+++ photowall/src/redux/reducer.js
@@ -1,13 +1,21 @@
-import posts from '../data/posts';
+import _posts from '../data/posts';
+import {combineReducers} from 'redux';
 
-const postReducer = function (state = posts, action) {
+function comments(state=[], action) {
+  switch (action.type) {
+    case 'ADD_COMMENT': return [...state, action.comment];
+    default: return state;
+  }
+}
 
+function posts(state = _posts, action) {
   switch (action.type) {
     case 'REMOVE_POST': return [...state.slice(0, action.index), ...state.slice(action.index + 1)];
     case 'ADD_POST': return [...state, action.post];
     default: return state;
   }
-
 }
 
-export default postReducer
+const rootReducer = combineReducers({posts, comments});
+
+export default rootReducer
